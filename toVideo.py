9f14b034811305ef308ade39ccb279a86ea3d77f
Akiva's Thinkpad <akivah@uchicago.edu>
2024-09-11 17:12:19 -0400

    i guess it works now

--- toVideo.py
+++ toVideo.py
@@ -1,12 +1,11 @@
 import os 
-import opencv4
+import cv2
 from PIL import Image 
 
 def generate_video(): 
-    image_folder = '.' # make sure to use your folder 
+    image_folder = 'outDir' # make sure to use your folder 
     video_name = 'mygeneratedvideo.avi'
-    os.chdir("C:\\Python\\Geekfolder2") 
-      
+    
     images = [img for img in os.listdir(image_folder) 
               if img.endswith(".jpg") or
                  img.endswith(".jpeg") or
@@ -16,18 +15,19 @@ def generate_video():
     # the image files ignoring others if any 
     print(images)  
   
-    frame = opencv.imread(os.path.join(image_folder, images[0])) 
+    frame = cv2.imread(os.path.join(image_folder, images[0])) 
   
     # setting the frame width, height width 
     # the width, height of first image 
     height, width, layers = frame.shape   
   
-    video = opencv.VideoWriter(video_name, 0, 1, (width, height))  
+    video = cv2.VideoWriter(video_name, 0, 1, (width, height))  
   
     # Appending the images to the video one by one 
     for image in images:  
-        video.write(opencv.imread(os.path.join(image_folder, image)))  
+        video.write(cv2.imread(os.path.join(image_folder, image)))  
       
     # Deallocating memories taken for window creation 
-    opencv.destroyAllWindows()  
+    cv2.destroyAllWindows()  
     video.release()  # releasing the video generated 
+generate_video()
