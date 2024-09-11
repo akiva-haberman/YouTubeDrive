140400e95f6ae83a8c5fe866bab70f9edafeaa62
Akiva's Thinkpad <akivah@uchicago.edu>
2024-09-11 13:29:16 -0400

    little clean up in decode

--- decode.py
+++ decode.py
@@ -7,6 +7,9 @@ from encode import index_to_coord
 
 # we are able to recover data from pixels!
 
+# explenation: basicly the metadata blocks, unlike the others, are one block to a byte. So if i want to record 20, let's say the resolution,
+# then it would be (20,0,0). Which makes them 3 times as long. so i need to skip extra when going to read the actual data. 
+MAGIC_META_NUMBER = 3
 
 def index_to_center_coord(index, blockSize, resX):
     row, col = index_to_coord(index, blockSize, resX)
@@ -21,26 +24,25 @@ def get_byte_arr(arr, blockSize, metaDataSize):
     # todo: look over indexing
     colors = arr[:,blockSize//2,:, blockSize//2,:]
     flat =  colors.flatten()
-    print(flat)
-    print(flat[metaDataSize:])
-    
-    return flat[metaDataSize * 3:]
+    return flat[metaDataSize * MAGIC_META_NUMBER:]
 
 # this is crude and would work if the pixels don't get messed up
 def get_file_specs(imgArr):
     blockSizeGuess = get_block_size(imgArr)
-    centerRow = imgArr[blockSizeGuess // 2]
     resX = len(imgArr[0])
-    # comment on this
-    metaDataSize = centerRow[blockSizeGuess * 2 + blockSizeGuess//2][0]
+    # first 2 block are used for getting block size
     read_index = 2
+    # get the center of the 3rd block
+    mdRow, mdCol = index_to_center_coord(read_index, blockSizeGuess, resX)
+    # the metadatasize is the 3 block in the image
+    metaDataSize = imgArr[mdRow][mdCol][0]
+    # record the metadata info
     metaData = [0] * (metaDataSize - read_index)
+
     for i, _ in enumerate(metaData) :
         row, col = index_to_center_coord(read_index, blockSizeGuess, resX)
         metaData[i] = imgArr[row][col][0]
         read_index+=1
-    print(metaData)
-    # print(f'blockSizeGuess = {blockSizeGuess}, centerRow = {centerRow}')
     return tuple(metaData)
 
 
@@ -55,6 +57,7 @@ def get_block_size(imgArr):
         temp = topRow[index]
         index+=1
     return index -1
+
 # should make default dict to handle errors and file types
 def num_to_extension(num):
     types = {
@@ -67,7 +70,7 @@ def num_to_extension(num):
 def writeImageToFile(inputFile, outName):
     with Image.open(inputFile) as im:
         imgArr = np.array(im)
-    metaDataSize, fileNum, resX, resY, blockSize = get_file_specs(imgArr)
+    metaDataSize, fileNum, _, _, blockSize = get_file_specs(imgArr)
     bytes = get_byte_arr(imgArr, blockSize, metaDataSize)
     outFile = outName + num_to_extension(fileNum)
     with open(outFile, 'wb') as f:
